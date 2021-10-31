5d5f87de08eb825a4ee9a725686ae08742d45e32
deveratancoder <93484729+deveratancoder@users.noreply.github.com>
2021-10-31 22:16:29 +0530

    added tokenization

---
+++ preprocessor/tokenization.py
@@ -0,0 +1,18 @@
+def CleanTokenize(df):
+    head_lines = list()
+    lines = df["text"].values.tolist()
+
+    for line in lines:
+        #line = clean_text(line)
+        # tokenize the text
+        tokens = word_tokenize(line)
+        # remove puntuations
+        table = str.maketrans('', '', string.punctuation)
+        stripped = [w.translate(table) for w in tokens]
+        # remove non alphabetic characters
+        words = [word for word in stripped if word.isalpha()]
+        stop_words = set(stopwords.words("english"))
+        # remove stop words
+        words = [w for w in words if not w in stop_words]
+        head_lines.append(words)
+    return head_lines
